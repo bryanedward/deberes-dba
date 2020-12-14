097d53ad9cdf762111966e2dee5d61ea25fe8e91
eddev <edwardbrian96@gmail.com>
2020-12-14 03:28:43 -0500

    recontruccion del sql server

--- deber1/consultas.sql
+++ deber1/consultas.sql
@@ -1,99 +1,122 @@
-CREATE TABLE clientes
-(
-    id_cliente INTEGER PRIMARY KEY,
-    nombre varchar(255),
-    telefono varchar(255),
-    correo varchar(255),
-    direccion varchar(255),
-    trabajo varchar(255)
+CREATE TABLE cliente(
+	cedula_cliente integer PRIMARY KEY,
+	nombre_cliente varchar(255),
+	apellido_cliente varchar(255),
+	telefono_cliente varchar(255),
+	correo_cliente varchar(255),
+	trabajo_cliente varchar(255),
+	ciudad_cliente varchar(255),
+	cod_sucursal varchar(255)
 );
 
-INSERT INTO clientes
-    (id_cliente, nombre, telefono, correo, direccion, trabajo)
-VALUES
-    (2, 'eduardo', '0983803020', 'eduardo89@gmail.com', 'barrio ursa', 'perchero en el local tia')
+insert into cliente values(1311354373, 'paula', 'mercedes', '0950025125', 'paula@gmail.com', 'comerciante', 'manta', 'AAOQK13')
+select * from cliente
 
 
-CREATE TABLE sucursales
-(
-    id_sucursal INTEGER PRIMARY KEY,
-    ciudad varchar(255),
-    direccion varchar(255),
-    telefono varchar(255),
+CREATE TABLE paquete(	
+	--creacion de la tabla paquete con referencias a la tabla ciudad, sucursal, cliente
+	cod_paquete varchar(255) PRIMARY KEY,
+	descrip_paquete varchar(255),
+	usuario_destinatario varchar(255),
+	valor_declarativo varchar(255),
+	fecha_ingreso date,
+	fecha_salida date,
+	ciudad_final integer FOREIGN KEY(ciudad_final) REFERENCES ciudad(id_ciudad),
+	cedula_cliente integer FOREIGN KEY(cedula_cliente) REFERENCES cliente(cedula_cliente),
+	cod_sucursal varchar(255) FOREIGN KEY(cod_sucursal) REFERENCES sucursal(cod_sucursal)
 );
+insert into paquete values ('ASDH2F2', 'caja de camiseta','juan martiz', 40.45, 
+'2008-11-11','2008-11-25', 9, 1311354373, 'OKOK')
 
-
-CREATE TABLE ciudades
-(
-    id_ciudad INTEGER PRIMARY KEY,
-    nombre varchar(255),
-    valor FLOAT
+CREATE TABLE ciudad(
+	--creacion de la tabla de ciudad
+	id_ciudad integer primary key IDENTITY(1,1),
+	nombre_ciudad varchar(255),
+	precio_envio float
 );
+insert into ciudad values ('jipijapa', 5.25)
 
 
-CREATE TABLE peso
-(
-    id_peso INTEGER PRIMARY KEY,
-    valor FLOAT
+
+create table sucursal(
+	-- creacion de la tabla sucursal
+	cod_sucursal varchar(255) primary key,
+	direccion_sucursal varchar(255),
+	telefono_sucursal varchar(255),
+	id_ciudad integer FOREIGN KEY(id_ciudad) REFERENCES ciudad(id_ciudad)
 );
+insert into sucursal values ('AAOQK13', 'calle b10', '0999042140', 6)
 
-CREATE TABLE paquetes
-(
-    id_paquete INTEGER PRIMARY KEY,
-    destinatario varchar(255),
-    urgencia BIT,
-    fecha DATE,
-    id_ciudad INTEGER,
-    id_peso INTEGER,
-    id_sucursal INTEGER,
+
+create table promocion(
+	--creacion de la tabla promocion
+	id_promocion integer primary key IDENTITY(1,1),
+	descrip_promocion varchar(255),
+	descuento_promocion float,
+	cod_sucursal varchar(255) foreign key (cod_sucursal) references sucursal(cod_sucursal)
+);
+INSERT INTO promocion values ('DESCUENTO NAVIDEÑO', 2.10, 'OKOK');
+
+create table peso (
+	dimension_paquete varchar(255) primary key,
+	peso_paquete float,
+	precio_peso float,
+);
+insert into peso values ('larga', 10, 7.15)
+
+create table urgencia(
+	nivel_urgencia varchar(255) primary key,
+	descrip_urgencia varchar(255),
+	precio_urgencia float
+);
+insert into urgencia values ('alto', 'estado de urgencia alta', 4.45);
+
+
+create table factura(
+	ruc_factura varchar(255) primary key,
+	fecha_factura date,
+	cod_paquete varchar (255) foreign key(cod_paquete) references paquete(cod_paquete),
+	cedula_cliente integer FOREIGN KEY(cedula_cliente) REFERENCES cliente(cedula_cliente),
+	id_ciudad integer FOREIGN KEY(id_ciudad) REFERENCES ciudad(id_ciudad),
+	dimension_paquete varchar(255) foreign key(dimension_paquete) references peso(dimension_paquete),
+	nivel_urgencia varchar(255) foreign key(nivel_urgencia) references urgencia(nivel_urgencia),
+	id_promocion integer foreign key(id_promocion) references promocion(id_promocion)
 )
-
--- CONSULTAR LAS TABLAS DE LA BASE DE DATOS
-SELECT *
-FROM sucursales
+insert into factura values ('AQWD2112', '2015-11-11', 'ASC132A','1911254271', 6, 'larga', 'alto', 1);
 
 
---CONTAR LA CANTIDAD DE PAQUETES QUE EXISTEN  ACTUALMENTE
-SELECT ( SELECT COUNT(*)
-    FROM paquetes) AS cantidad_total_de_paquetes
+--primer pregunta
+SELECT ( SELECT COUNT(*)​
+FROM paquete) AS cantidad_total_de_paquetes
+
+--segunda pregunta
+select cliente.cedula_cliente, cliente.nombre_cliente,
+promocion.descrip_promocion ,promocion.descuento_promocion,
+CONCAT(SUM(urgencia.precio_urgencia + peso.precio_peso + ciudad.precio_envio),'$') valor_normal,
+CONCAT(SUM(urgencia.precio_urgencia + peso.precio_peso + ciudad.precio_envio) - promocion.descuento_promocion,'$') valor_descuento
+from cliente inner join factura
+on factura.cedula_cliente = cliente.cedula_cliente inner join urgencia
+on urgencia.nivel_urgencia = factura.nivel_urgencia inner join peso
+on peso.dimension_paquete = factura.dimension_paquete inner join ciudad
+on ciudad.id_ciudad = factura.id_ciudad inner join promocion
+on promocion.id_promocion= factura.id_promocion
+group by cliente.cedula_cliente, promocion.descuento_promocion, 
+cliente.nombre_cliente,promocion.descrip_promocion 
 
 
--- ¿Cuáles son los costos de cada cliente que ha ocupado el servicio?
-SELECT ciudades.nombre as ciudad_envio, clientes.nombre as remitente,
-    CONCAT(SUM(ciudades.valor), '$') ganancia_total
-FROM clientes INNER JOIN paquetes on paquetes.id_ciudad = clientes.id_cliente
-    INNER JOIN ciudades on ciudades.id_ciudad = paquetes.id_ciudad
-GROUP BY ciudades.nombre, clientes.nombre
+--tercer pregunta
+select * from paquete where CAST(fecha_ingreso as date) = CAST('2019-11-11' as date)
 
 
---consultar los paquetes enviados en una fecha especifica
--- mostrando el costo de envio
-SELECT destinatario , CAST(fecha AS datetime ) , clientes.nombre as remitente,
-    sucursales.ciudad AS ciudad_de_la_sucursal, ciudades.nombre as ciudad_envio,
-    CONCAT(ciudades.valor, '$') AS costo_envio_ciudad
-FROM paquetes
-    INNER JOIN clientes ON clientes.id_cliente = paquetes.id_cliente
-    INNER JOIN sucursales ON sucursales.id_sucursal = paquetes.id_sucursal
-    INNER JOIN ciudades ON ciudades.id_ciudad = paquetes.id_ciudad
-WHERE CAST(fecha AS date)  = CAST('2020-12-12' AS date)
+
+-- cuarta pregunta
+select nombre_ciudad, CONCAT(precio_envio, '$') precio_envio, paquete.descrip_paquete 
+from paquete inner join ciudad on ciudad.id_ciudad = 
+paquete.ciudad_final
+where nombre_ciudad = 'guayaquil' group by precio_envio, nombre_ciudad, paquete.descrip_paquete;
 
 
---¿Cuántos paquetes son enviados a una determinada ciudad y cuanto es la ganancia que se obtuvo con los viajes?​
--- consultar todos los paquetes que son enviados a quito
-SELECT id_paquete, destinatario, fecha , nombre AS ciudad_envio, valor
-FROM paquetes
-    INNER JOIN ciudades
-    ON paquetes.id_ciudad = ciudades.id_ciudad
-WHERE ciudades.nombre LIKE 'quito';
-
---obtener la ganancia total de los que se obtuvo de los paquetes enviados a una determinada
--- ciudad
-SELECT ciudades.nombre , valor,
-    CONCAT(SUM(ciudades.valor), '$') ganancia_total
-FROM paquetes
-    INNER JOIN ciudades
-    ON paquetes.id_ciudad = ciudades.id_ciudad INNER JOIN clientes ON paquetes.id_cliente = clientes.id_cliente
-WHERE ciudades.nombre LIKE 'manta'
-GROUP BY ciudades.nombre, ciudades.valor
-
-​
+--quinta pregunta
+select top 1 *
+  from paquete
+ order by fecha_ingreso desc
